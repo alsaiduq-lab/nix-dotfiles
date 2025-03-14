ffcf2e3225a1f670b54ceff15caf2a6e7cca696b
alsaiduq-lab <riiidge.racer@gmail.com>
2025-03-14 04:43:57 -0600

    prevents an incomplete system

--- modules/fish.nix
+++ modules/fish.nix
@@ -1,9 +1,12 @@
 { config, pkgs, lib, ... }:
 
+let
+  fish-rust = pkgs.callPackage ../pkgs/fish-rust { };
+in
 {
   programs.fish.enable = true;
-
   environment.systemPackages = with pkgs; [
+    fish-rust
     starship
     fzf
     bat
