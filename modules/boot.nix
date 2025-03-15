eed2bf8a4ac3883322bd9532fe60e252855aded5
alsaiduq-lab <riiidge.racer@gmail.com>
2025-03-15 04:52:43 -0600

    shouldve been here really

--- modules/boot.nix
+++ modules/boot.nix
@@ -5,6 +5,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.memtest86.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.timeout = 5;
   # Create the traditional /bin directory with a symlink to bash for scripts
   system.activationScripts.binbash = {
