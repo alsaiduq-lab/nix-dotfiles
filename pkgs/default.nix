3e8e5ba6dff324f788dee32207054604f8cffb41
alsaiduq-lab <riiidge.racer@gmail.com>
2025-03-20 22:23:18 -0600

    should be good

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,22 +2,26 @@
   pkgs ? import <nixpkgs> {},
   lib ? pkgs.lib,
 }: let
+  python-opencv-headless = pkgs.callPackage ./python-opencv-headless {
+    inherit (pkgs) lib fetchPypi;
+    python310Packages = pkgs.python310.pkgs;
+  };
+
   python-pymatting = pkgs.callPackage ./python-pymatting {
     inherit (pkgs) lib fetchPypi;
     python310Packages = pkgs.python310.pkgs;
   };
-  python-opencv-headless = pkgs.callPackage ./python-opencv-headless {
+
+  python-rembg = pkgs.callPackage ./python-rembg {
     inherit (pkgs) lib fetchPypi;
+    inherit python-pymatting python-opencv-headless;
     python310Packages = pkgs.python310.pkgs;
   };
 in {
   fish-rust = pkgs.callPackage ./fish-rust {};
   python-pymatting = python-pymatting;
-  python-rembg = pkgs.callPackage ./python-rembg {
-    inherit (pkgs) lib fetchPypi;
-    python310Packages = pkgs.python310.pkgs;
-    inherit python-pymatting;
-  };
+  python-opencv-headless = python-opencv-headless;
+  python-rembg = python-rembg;
 
   tokyo-night-gtk = pkgs.callPackage ./tokyo-night {
     inherit (pkgs) lib stdenv fetchFromGitHub gtk-engine-murrine;
