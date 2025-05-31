ba6d89960c94f5c1e29eea5aafb102791c74d0c9
alsaiduq-lab <riiidge.racer@gmail.com>
2025-05-31 07:11:10 -0600

    fixes node

--- modules/default.nix
+++ modules/default.nix
@@ -24,6 +24,8 @@
     ./cups.nix
   ];
 
+  npm.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
