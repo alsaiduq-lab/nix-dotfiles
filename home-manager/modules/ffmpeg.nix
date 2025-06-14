32e8236f36af02768d385a8e66b1bb0034663f65
alsaiduq-lab <riiidge.racer@gmail.com>
2025-06-14 12:28:08 -0600

    Squash merge dev into master

--- home-manager/modules/ffmpeg.nix
+++ home-manager/modules/ffmpeg.nix
@@ -1,25 +1,5 @@
-{pkgs, ...}: let
-  x11ffmpeg = pkgs.ffmpeg-full.overrideAttrs (old: {
-    buildInputs =
-      (old.buildInputs or [])
-      ++ [
-        pkgs.xorg.libxcb
-        pkgs.xorg.libX11
-        pkgs.xorg.xcbutil
-        pkgs.xorg.xcbutilimage
-        pkgs.xorg.xcbutilkeysyms
-        pkgs.xorg.xcbutilwm
-      ];
-    configureFlags =
-      (old.configureFlags or [])
-      ++ [
-        "--enable-libxcb"
-        "--enable-libxcb-shm"
-        "--enable-libxcb-xfixes"
-      ];
-  });
-in {
+{pkgs, ...}: {
   home.packages = [
-    x11ffmpeg
+    (pkgs.ffmpeg-full.override {withXcb = true;})
   ];
 }
