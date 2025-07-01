bf16581c61af54d1e1d74e84a73a9488dc698c9d
alsaiduq-lab <riiidge.racer@gmail.com>
2025-06-30 21:22:01 -0600

    originally default.nix

--- hosts/system-packages.nix
+++ hosts/system-packages.nix
@@ -1,78 +1,38 @@
-# TODO: cleanout and repopulate individual packages better
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    obs-studio
-    spotify
-    flameshot
-    mpv
-    linuxHeaders
-    appimage-run
-    freetype.dev
-    pixman
-    bitwarden
-    udiskie
-    maim
-    mercurial
-    go
-    go-tools
-    gopls
-    gcc
-    gnumake
-    cmake
-    ninja
-    binutils
-    gdb
-    pkg-config
-    autoconf
-    automake
-    libtool
-    nasm
-    elixir
-    swift
-    zig
-    dbus.dev
-    pkg-config
-    ghc
-    cacert
-    cabal-install
-    stack
-    jdk17
-    maven
-    gradle
-    visualvm
-    wget
-    curl
-    tree
-    mesa
-    libglvnd
-    socat
-    gawk
-    nmap
-    psmisc
-    unzip
-    fd
-    jq
-    bash
-    slop
-    yt-dlp # might remove for source built version
-    httpie
-    wrk
-    cloudflared
-    hashcat
-    nix-prefetch-git
-    openssl
-    openssl.dev
-    libxml2
-    zlib
-    zlib.dev
-    postgresql
-    sqlite
-    redis
-    sqls
-    cachix
-    texlive.combined.scheme-full
-    imagemagick
-    poppler_utils
-    ghostscript
+  imports = [
+    ./modules/audio.nix
+    ./modules/bluetooth.nix
+    ./modules/boot.nix
+    ./modules/docker.nix
+    ./modules/env.nix
+    ./modules/fonts.nix
+    ./modules/net.nix
+    ./modules/nixos.nix
+    ./modules/nvidia.nix
+    ./modules/steam.nix
+    ./modules/timezone.nix
+    ./modules/user.nix
+    ./modules/python.nix
+    ./modules/npm.nix
+    ./modules/tailscale.nix
+    ./modules/udiskie.nix
+    ./modules/ld.nix
+    ./modules/x11.nix
+    ./modules/i3-xfce.nix
+    ./modules/ollama.nix
+    ./modules/cups.nix
+    ./modules/rust.nix
+    ./modules/java.nix
+    ./modules/go.nix
+    ./modules/core.nix
+    ./modules/libs.nix
   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 }
