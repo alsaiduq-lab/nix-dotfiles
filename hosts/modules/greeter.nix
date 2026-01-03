94dce4f2b8f6cd53fce34a83d050185ebdc73611
alsaiduq-lab <riiidge.racer@gmail.com>
2026-01-03 01:21:27 -0700

    fixes the login process
    there's still some PAM issue left. it's not really a big deal it seems

--- hosts/modules/greeter.nix
+++ hosts/modules/greeter.nix
@@ -12,4 +12,18 @@
     compositor.name = "hyprland";
     configHome = "/home/${config.theme.user}";
   };
+
+  users.users.greeter = {
+    isSystemUser = true;
+    group = "greeter";
+    home = "/var/lib/greeter";
+    createHome = true;
+  };
+
+  users.groups.greeter = {};
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/greeter/.config/systemd/user 0755 greeter greeter -"
+    "L+ /var/lib/greeter/.config/systemd/user/xdg-desktop-portal-hyprland.service - - - - /dev/null"
+  ];
 }
