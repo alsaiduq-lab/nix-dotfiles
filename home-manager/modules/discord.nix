9fd98381ea68f6b621dc5965a0e04c3d07ca5626
alsaiduq-lab <riiidge.racer@gmail.com>
2026-01-03 20:18:21 -0700

    switched to nixcord

--- home-manager/modules/discord.nix
+++ home-manager/modules/discord.nix
@@ -1,6 +1,85 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    vesktop
-    discord
-  ];
+{
+  pkgs,
+  config,
+  ...
+}: {
+  programs.nixcord = {
+    enable = true;
+    discord.enable = true;
+    vesktop.enable = true;
+    equibop.enable = true;
+    config = {
+      autoUpdateNotification = true;
+      notifyAboutUpdates = true;
+
+      plugins = {
+        AutoDNDWhilePlaying = {
+          enable = true;
+          excludeInvisible = true;
+        };
+        BlurNSFW = {
+          enable = true;
+        };
+        ClearURLs = {
+          enable = true;
+        };
+        OnePingPerDM = {
+          enable = true;
+          allowMentions = true;
+          ignoreUsers = "Wumpus"; # die
+        };
+        RPCEditor = {
+          enable = true;
+        };
+        ReviewDB = {
+          enable = true;
+        };
+        anonymiseFileNames = {
+          enable = true;
+          consistent = "killallWumpus";
+        };
+        autoZipper = {
+          enable = true;
+        };
+        betterInvites = {
+          enable = true;
+        };
+        biggerStreamPreview = {
+          enable = true;
+        };
+        blockKeywords = {
+          enable = true;
+          useRegex = true;
+          blockedWords = "Wumpus";
+        };
+        customIdle = {
+          enable = true;
+        };
+        expressionCloner = {
+          enable = true;
+        };
+        fixFileExtensions = {
+          enable = true;
+        };
+        fixYoutubeEmbeds = {
+          enable = true;
+        };
+        forceOwnerCrown = {
+          enable = true;
+        };
+        reverseImageSearch = {
+          enable = true;
+        };
+        richMagnetLinks = {
+          enable = true;
+        };
+        sekaiStickers = {
+          enable = true;
+        };
+        sendTimestamps = {
+          enable = true;
+        };
+      };
+    };
+  };
 }
