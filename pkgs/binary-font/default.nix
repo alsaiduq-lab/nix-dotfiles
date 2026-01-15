008d5c15c01731c6dbcfa1333b724ca0ff088814
alsaiduq-lab <riiidge.racer@gmail.com>
2026-01-15 13:13:37 -0700

    fixes

--- pkgs/binary-font/default.nix
+++ pkgs/binary-font/default.nix
@@ -2,25 +2,24 @@
   pkgs,
   lib,
   ...
-}: {
-  binary-clock-font = pkgs.stdenv.mkDerivation {
-    pname = "binary-clock-font";
-    version = "1.0.0";
-    src = pkgs.fetchurl {
-      url = "https://github.com/jamessouth/polybar-binary-clock-fonts/raw/master/BinaryClockBoldMono.ttf";
-      sha256 = "0vxy23zr8r8faa5s7vy5bf8z2q7my39ghmd9ilk7aww9wqsrsjqx";
-    };
-    dontUnpack = true;
-    installPhase = ''
-      mkdir -p $out/share/fonts/truetype
-      cp $src $out/share/fonts/truetype/BinaryClockBoldMono.ttf
-    '';
-    meta = with lib; {
-      description = "A monospaced bold font for binary clocks";
-      homepage = "https://github.com/jamessouth/polybar-binary-clock-fonts";
-      license = licenses.ofl;
-      platforms = platforms.all;
-      maintainers = with maintainers; [cobray];
-    };
+}:
+pkgs.stdenv.mkDerivation {
+  pname = "binary-clock-font";
+  version = "1.0.0";
+  src = pkgs.fetchurl {
+    url = "https://github.com/jamessouth/polybar-binary-clock-fonts/raw/master/BinaryClockBoldMono.ttf";
+    sha256 = "0vxy23zr8r8faa5s7vy5bf8z2q7my39ghmd9ilk7aww9wqsrsjqx";
+  };
+  dontUnpack = true;
+  installPhase = ''
+    mkdir -p $out/share/fonts/truetype
+    cp $src $out/share/fonts/truetype/BinaryClockBoldMono.ttf
+  '';
+  meta = with lib; {
+    description = "A monospaced bold font for binary clocks";
+    homepage = "https://github.com/jamessouth/polybar-binary-clock-fonts";
+    license = licenses.ofl;
+    platforms = platforms.all;
+    maintainers = ["cobray"];
   };
 }
--- pkgs/clear-sans/default.nix
+++ pkgs/clear-sans/default.nix
@@ -2,26 +2,25 @@
   pkgs,
   lib,
   ...
-}: {
-  clear-sans = pkgs.stdenv.mkDerivation {
-    pname = "clear-sans";
-    version = "1.0";
-    src = pkgs.fetchFromGitHub {
-      owner = "intel";
-      repo = "clear-sans";
-      rev = "main";
-      sha256 = "sha256-+xdetdE3Z4NlrWiEYCUO5bmf06g/+p5blkkNk+XcruQ=";
-    };
-    installPhase = ''
-      mkdir -p $out/share/fonts/truetype
-      cp TTF/*.ttf $out/share/fonts/truetype/
-    '';
-    meta = with lib; {
-      description = "Clear Sans font";
-      homepage = "https://github.com/intel/clear-sans";
-      license = licenses.asl20;
-      maintainers = [cobray];
-      platforms = platforms.all;
-    };
+}:
+pkgs.stdenv.mkDerivation {
+  pname = "clear-sans";
+  version = "1.0";
+  src = pkgs.fetchFromGitHub {
+    owner = "intel";
+    repo = "clear-sans";
+    rev = "main";
+    sha256 = "sha256-+xdetdE3Z4NlrWiEYCUO5bmf06g/+p5blkkNk+XcruQ=";
+  };
+  installPhase = ''
+    mkdir -p $out/share/fonts/truetype
+    cp TTF/*.ttf $out/share/fonts/truetype/
+  '';
+  meta = with lib; {
+    description = "Clear Sans font";
+    homepage = "https://github.com/intel/clear-sans";
+    license = licenses.asl20;
+    maintainers = [cobray];
+    platforms = platforms.all;
   };
 }
