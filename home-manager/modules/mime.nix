06e43c37d8601c6156ba0ec40fe36e4a08c1958d
alsaiduq-lab <riiidge.racer@gmail.com>
2026-07-22 20:38:18 -0600

    fixed
    ignore the *

--- home-manager/modules/mime.nix
+++ home-manager/modules/mime.nix
@@ -1,24 +1,34 @@
-{settings, ...}: {
+{
+  lib,
+  pkgs,
+  settings,
+  ...
+}: let
+  associations = desktopFile: desktopName: let
+    mimeLine =
+      lib.findFirst
+      (line: lib.hasPrefix "MimeType=" line)
+      (throw "no MimeType= line in ${desktopFile}")
+      (lib.splitString "\n" (builtins.readFile desktopFile));
+  in
+    lib.genAttrs
+    (lib.filter (type: type != "") (lib.splitString ";" (lib.removePrefix "MimeType=" mimeLine)))
+    (_: desktopName);
+in {
   xdg.mimeApps = {
     enable = true;
-    defaultApplications = {
-      "inode/directory" = "org.kde.dolphin.desktop";
-      "image/*" = "org.kde.gwenview.desktop";
-      "image/png" = "org.kde.gwenview.desktop";
-      "image/jpeg" = "org.kde.gwenview.desktop";
-      "image/gif" = "org.kde.gwenview.desktop";
-      "image/webp" = "org.kde.gwenview.desktop";
-      "image/bmp" = "org.kde.gwenview.desktop";
-      "image/svg+xml" = "org.kde.gwenview.desktop";
-      "image/tiff" = "org.kde.gwenview.desktop";
-      "text/html" = "${settings.Browser}.desktop";
-      "x-scheme-handler/http" = "${settings.Browser}.desktop";
-      "x-scheme-handler/https" = "${settings.Browser}.desktop";
-      "application/pdf" = "${settings.Browser}.desktop";
-      "video/*" = "mpv.desktop";
-      "audio/*" = "mpv.desktop";
-      "text/plain" = "${settings.Editor}.desktop";
-    };
+    defaultApplications =
+      associations "${pkgs.mpv}/share/applications/mpv.desktop" "mpv.desktop"
+      // associations "${pkgs.kdePackages.gwenview}/share/applications/org.kde.gwenview.desktop" "org.kde.gwenview.desktop"
+      // {
+        "inode/directory" = "org.kde.dolphin.desktop";
+        "text/html" = "${settings.Browser}.desktop";
+        "x-scheme-handler/http" = "${settings.Browser}.desktop";
+        "x-scheme-handler/https" = "${settings.Browser}.desktop";
+        "application/pdf" = "${settings.Browser}.desktop";
+        "text/plain" = "${settings.Editor}.desktop";
+      };
   };
+
   xdg.configFile."mimeapps.list".force = true;
 }
